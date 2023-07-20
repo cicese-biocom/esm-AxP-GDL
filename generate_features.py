4281f45d15e23e21947f0507fd34410ca4124b3a
Greneter Cordoves Delgado <grenetercordovesdelgado@gmail.com>
2023-07-19 21:07:33 -0700

    Change generate_features.py to add hhblits

--- generate_features.py
+++ generate_features.py
@@ -1,4 +1,5 @@
 import os
+import utils.hhblits_search as hh
 import yaml
 import argparse
 
@@ -7,7 +8,11 @@ with open("config.yaml", 'r') as f:
     cfg = yaml.load(f, Loader=yaml.FullLoader)
 
 # Tools
+hhblits = cfg['hhblits']
 rosetta = cfg['rosetta']
+
+# Database
+uniclust = cfg['uniclust']
 
 # Models
 rosetta_model = cfg['rosetta_model']
@@ -17,6 +22,8 @@ def generate_features(args):
     """
     """
     feas = args.feas
+    if 'HHM' in feas:
+        hh.run(hhblits, args.hhm_ifasta, args.hhm_oa3m, args.hhm_ohhm, args.hhm_tmp, uniclust)
 
     if 'NPZ' in feas:
         rosetta_cmd = 'python ' + rosetta + ' ' + \
@@ -28,13 +35,22 @@ if __name__ == '__main__':      # Si generate_features.py se ejecuta directament
     # generate contact map, esm2 features before train and test model.
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-feas', type=str, nargs='+', default=['NPZ'], help='Feature names')
+    parser.add_argument('-feas', type=str, nargs='+', default=['HHM', 'NPZ'], help='Feature names')
+
+    # HHblits parameters
+    parser.add_argument('-hhm_ifasta', type=str, default='example2/test.fasta',
+                        help='Input a file with fasta format for hhblits search')
+    parser.add_argument('-hhm_oa3m', type=str, default='example2/a3m/',
+                        help='Output folder saving .a3m files')
 
     # trRosetta parameters
-    parser.add_argument('-tr_ia3m', type=str, default='example/a3m/',
+    parser.add_argument('-tr_ia3m', type=str, default='example2/a3m/',
                         help='Input folder saving .a3m files')
-    parser.add_argument('-tr_onpz', type=str, default='example/npz/',
+    parser.add_argument('-tr_onpz', type=str, default='example2/npz/',
                         help='Output folder saving .npz files')
+    parser.add_argument('-hhm_ohhm', type=str, default='example2/hhm/',
+                        help='Output folder saving .hhm files')
+    parser.add_argument('-hhm_tmp', type=str, default='example2/tmp/', help='Temp folder')
 
     args = parser.parse_args()
 
