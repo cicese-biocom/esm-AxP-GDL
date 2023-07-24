0fdf30d9f4dbb6d3110301982f2ce3861866a647
Greneter Cordoves Delgado <grenetercordovesdelgado@gmail.com>
2023-07-24 09:35:02 -0700

    Change encoding_methods.py to add esm2 model

--- utils/encoding_methods.py
+++ utils/encoding_methods.py
@@ -1,5 +1,38 @@
 import os
 import numpy as np
+import torch as torch
+
+def esm2_embbeding(ids, esm2_dir):
+    """
+    parser esm2 features
+    """
+    if esm2_dir[-1] != '/': esm2_dir += '/'
+    esm2_fs = os.listdir(esm2_dir)
+    res = []
+    for id in ids:
+        name = id
+        if id[0] == '>': name = id[1:]
+        file_pt = name + '.pt'
+        assert file_pt in esm2_fs
+
+        #  Get the full path to the .pt file
+        file_pt_path = os.path.join(esm2_dir, file_pt)
+
+        # Load the .pt file
+        emb_pt = torch.load(file_pt_path)
+
+        assert 'representations' in emb_pt
+
+        # representations is a dictionary object with a single key, whose key name varies depending on the layer number
+        # of the esm2 model used.representations is a dictionary object with a single key, whose key name varies
+        # depending on the layer number of the esm2 model used.
+        representations = emb_pt["representations"]
+        key = list(representations.keys())[0]
+        tensor = representations[key]
+
+        res.append(tensor.numpy())
+
+    return res
 
 
 def onehot_encoding(seqs):
