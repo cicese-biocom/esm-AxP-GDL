4a56cb8592dd5a5f02ba8c690a99380afe4c0e35
Greneter Cordoves Delgado <grenetercordovesdelgado@gmail.com>
2023-07-24 09:37:40 -0700

    Change generate_features.py to add esm2 model

--- generate_features.py
+++ generate_features.py
@@ -1,5 +1,6 @@
 import os
 import utils.hhblits_search as hh
+import esm
 import yaml
 import argparse
 
@@ -10,13 +11,14 @@ with open("config.yaml", 'r') as f:
 # Tools
 hhblits = cfg['hhblits']
 rosetta = cfg['rosetta']
+esm2 = cfg['esm2']
 
 # Database
 uniclust = cfg['uniclust']
 
 # Models
 rosetta_model = cfg['rosetta_model']
-
+esm2_model = cfg['esm2_model']
 
 def generate_features(args):
     """
@@ -30,27 +32,51 @@ def generate_features(args):
                       args.tr_ia3m + ' ' + args.tr_onpz + ' -m ' + rosetta_model
         os.system(rosetta_cmd)
 
+    if 'ESM2' in feas:
+        esm2_cmd = 'python' + ' ' + esm2 + ' ' + esm2_model + ' ' + args.fasta_file + ' ' + args.output_dir + \
+                   ' ' + '--repr_layers [-1] --include per_tok'
+
+        os.system(esm2_cmd)
+
 
 if __name__ == '__main__':      # Si generate_features.py se ejecuta directamente como un script
     # generate contact map, esm2 features before train and test model.
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-feas', type=str, nargs='+', default=['HHM', 'NPZ'], help='Feature names')
+    parser.add_argument('-feas', type=str, nargs='+', default=['HHM', 'NPZ', 'ESM2'], help='Feature names')
 
     # HHblits parameters
-    parser.add_argument('-hhm_ifasta', type=str, default='example2/test.fasta',
+    parser.add_argument('-hhm_ifasta', type=str, default='data/train_data/negative/example_neg.fasta',
                         help='Input a file with fasta format for hhblits search')
-    parser.add_argument('-hhm_oa3m', type=str, default='example2/a3m/',
+    parser.add_argument('-hhm_oa3m', type=str, default='data/train_data/negative/a3m/',
                         help='Output folder saving .a3m files')
 
     # trRosetta parameters
-    parser.add_argument('-tr_ia3m', type=str, default='example2/a3m/',
+    parser.add_argument('-tr_ia3m', type=str, default='data/train_data/negative/a3m/',
                         help='Input folder saving .a3m files')
-    parser.add_argument('-tr_onpz', type=str, default='example2/npz/',
+    parser.add_argument('-tr_onpz', type=str, default='data/train_data/negative/npz/',
                         help='Output folder saving .npz files')
-    parser.add_argument('-hhm_ohhm', type=str, default='example2/hhm/',
+    parser.add_argument('-hhm_ohhm', type=str, default='data/train_data/negative/hhm/',
                         help='Output folder saving .hhm files')
-    parser.add_argument('-hhm_tmp', type=str, default='example2/tmp/', help='Temp folder')
+    parser.add_argument('-hhm_tmp', type=str, default='data/train_data/negative/tmp/', help='Temp folder')
+
+    # esm2 parameters
+    parser.add_argument('-model_location', type=str, default=esm2_model,
+                        help='PyTorch model file')
+    parser.add_argument('-fasta_file', type=str, default='data/train_data/negative/example_neg.fasta',
+                        help='FASTA file on which to extract representations')
+    parser.add_argument('-output_dir', type=str, default='data/train_data/negative/esm2',
+                        help='output directory for extracted representations')
+    parser.add_argument('-toks_per_batch', type=str, default='4096',
+                        help='maximum batch size')
+    parser.add_argument('-repr_layers', type=str, default='33', # final only
+                        help='layers indices from which to extract representations (0 to num_layers, inclusive)')
+    parser.add_argument('-include', type=str, default='per_tok',
+                        help='specify which representations to return')
+    parser.add_argument('-truncation_seq_length', type=str, default='1022',
+                        help='truncate sequences longer than the given value')
+    parser.add_argument('-nogpu', type=str,
+                        help='Do not use GPU even if available')
 
     args = parser.parse_args()
 
