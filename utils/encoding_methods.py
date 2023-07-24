0569f242d3a0e8a8c0e2a76f65505cd6d91a7368
Greneter Cordoves Delgado <grenetercordovesdelgado@gmail.com>
2023-07-24 11:29:44 -0700

    Change encoding_methods.py to remove onehot_encoding, position_encoding and add methods

--- utils/encoding_methods.py
+++ utils/encoding_methods.py
@@ -35,56 +35,6 @@ def esm2_embbeding(ids, esm2_dir):
     return res
 
 
-def onehot_encoding(seqs):
-    residues = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K',
-                'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V']
-
-    encoding_map = np.eye(len(residues))
-
-    residues_map = {}
-    for i, r in enumerate(residues):
-        residues_map[r] = encoding_map[i]
-
-    res_seqs = []
-
-    for seq in seqs:
-        tmp_seq = [residues_map[r] for r in seq]
-        res_seqs.append(np.array(tmp_seq))
-
-    return res_seqs
-
-
-def position_encoding(seqs):
-    """
-    Position encoding features introduced in "Attention is all your need",
-    the b is changed to 1000 for the short length of peptides.
-    """
-    d = 20
-    b = 1000
-    res = []
-    for seq in seqs:
-        N = len(seq)
-        value = []
-        for pos in range(N):
-            tmp = []
-            for i in range(d // 2):
-                tmp.append(pos / (b ** (2 * i / d)))
-            value.append(tmp)
-        value = np.array(value)
-        pos_encoding = np.zeros((N, d))
-        pos_encoding[:, 0::2] = np.sin(value[:, :])
-        pos_encoding[:, 1::2] = np.cos(value[:, :])
-        res.append(pos_encoding)
-    return res
-
-
-def add(e1, e2):
-    res = []
-    for i in range(len(e1)):
-        res.append(e1[i] + e2[i])
-    return res
-
-
 def cat(*args):
     """
     :param args: feature matrices
@@ -94,7 +44,3 @@ def cat(*args):
         for i in range(len(matrix)):
             res[i] = np.hstack((res[i], matrix[i]))
     return res
-
-
-if __name__ == '__main__':
-    position_encoding(['ARFGD', 'AAAAAA'])
