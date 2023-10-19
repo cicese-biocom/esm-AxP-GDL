888778ce3ab552cf508cc13f76b47d5a2da55219
Greneter Cordoves Delgado <greneter@cicese.edu.mx>
2023-10-19 15:25:51 -0700

    Change in residues_level_features_encoding.py: Remove normalize_embedding parameter.

--- graph/residues_level_features_encoding.py
+++ graph/residues_level_features_encoding.py
@@ -2,7 +2,7 @@ import numpy as np
 from models.esm2.esm2_representation import *
 
 
-def esm2_derived_features(data, esm2_representation, normalize_embedding):
+def esm2_derived_features(data, esm2_representation):
     """
     :param data: (ids: sequences identifier, sequences: sequences itself)
     :param esm2_representation: name of the esm2 representation to be used
@@ -19,7 +19,7 @@ def esm2_derived_features(data, esm2_representation, normalize_embedding):
             reduced_features = model_info["reduced_features"]
             reduced_features = [x - 1 for x in reduced_features]
 
-            embeddings = get_embeddings(data, model_name, reduced_features, normalize_embedding)
+            embeddings = get_embeddings(data, model_name, reduced_features)
 
             if len(residual_level_features) == 0:
                 residual_level_features = np.array(embeddings, dtype=object).copy()
