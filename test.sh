e47323bfa6c9fe74dc8e3067d8b10b3b2589fb4c
Greneter Cordoves Delgado <greneter@cicese.edu.mx>
2023-10-28 02:43:26 -0700

    Changes in test.sh to run test.py from the console

--- test.sh
+++ test.sh
@@ -1,16 +1,14 @@
 #! /bin/bash
 
-pos=data/test_data/positive
-neg=data/test_data/negative
-
-mkdir -p $pos 
-mkdir -p $neg
-
-cp datasets/independent\ test\ datasets/XUAMP/XU_AMP.fasta $pos/XU_AMP.fasta
-cp datasets/independent\ test\ datasets/XUAMP/XU_nonAMP.fasta $neg/XU_nonAMP.fasta
-
-python generate_features.py -hhm_ifasta $pos/XU_AMP.fasta -hhm_oa3m $pos/a3m/ -hhm_ohhm $pos/hhm/ -hhm_tmp $pos/tmp/ -tr_ia3m $pos/a3m/ -tr_onpz $pos/npz/ -pssm_ifasta $pos/XU_AMP.fasta -pssm_opssm $pos/pssm/
-python generate_features.py -hhm_ifasta $neg/XU_nonAMP.fasta -hhm_oa3m $neg/a3m/ -hhm_ohhm $neg/hhm/ -hhm_tmp $neg/tmp/ -tr_ia3m $neg/a3m/ -tr_onpz $neg/npz/ -pssm_ifasta $neg/XU_nonAMP.fasta -pssm_opssm $neg/pssm/
-
-python test.py  -pos_t $pos/XU_AMP.fasta -pos_npz $pos/npz/ -neg_t $neg/XU_nonAMP.fasta -neg_npz $neg/npz/ \
-                -save saved_models/samp.model
+python test.py \
+    -dataset datasets/DeepAVPpred/DeepAVPpred.csv \
+    -esm2_representation esm2_t6 \
+    -tertiary_structure_method esmfold \
+    -tertiary_structure_path datasets/DeepAVPpred/ESMFold_pdbs/ \
+    -tertiary_structure_operation_mode load \
+    -b 512 \
+    -save output_models/checkpoint_epoch20.pt \
+    -drop 0.5 \
+    -hd 64 \
+    -heads 8 \
+    -d 20
