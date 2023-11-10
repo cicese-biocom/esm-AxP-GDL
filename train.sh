9639b7767ee538030182dbddbed70587a4cf2e7d
Greneter Cordoves Delgado <greneter@cicese.edu.mx>
2023-11-10 01:12:03 -0800

    Change in train.sh to perform the experimentation

--- train.sh
+++ train.sh
@@ -1,14 +1,19 @@
-#! /bin/bash
+#!/bin/bash
+
+e=2
+hd=64
+d=10
+esm2_representation="esm2_t6"
+path_to_save_models="output_models/64_10_t8/"
 
 python train.py \
-    -dataset datasets/DeepAVPpred/DeepAVPpred.csv \
-    -esm2_representation esm2_t6 \
-    -tertiary_structure_method esmfold \
-    -tertiary_structure_path datasets/DeepAVPpred/ESMFold_pdbs/ \
-    -tertiary_structure_operation_mode load \
-    -e 20 \
-    -b 512 \
-    -hd 64 \
-    -save output_models/ \
-    -heads 8 \
-    -d 20
+    --dataset datasets/AMPDiscover/AMPDiscover.csv \
+    --esm2_representation "$esm2_representation" \
+    --tertiary_structure_method esmfold \
+    --tertiary_structure_path datasets/AMPDiscover/ESMFold_pdbs/ \
+    --tertiary_structure_load_pdbs \
+    --e "$e" \
+    --hd "$hd" \
+    --path_to_save_models "$path_to_save_models" \
+    --d "$d" \
+    --add_self_loop
