534e7ebbed536f45e6fc7729c8939ed53ca96392
Greneter Cordoves Delgado <greneter@cicese.edu.mx>
2024-03-29 23:53:40 -0700

    docs(example): Update parameters

--- test.sh
+++ test.sh
@@ -1,23 +1,39 @@
 #! /bin/bash
 
-hd=128
-d=10
-esm2_representation="esm2_t36"
-trained_model_path="output_models/AMPDiscover/amp_esmt36_d10_hd128/amp_esmt36_d10_hd128.pt"
-log_file_name="TestLog_AMPDiscover"
-test_result_file_name="TestResult_AMPDiscover"
+dataset="datasets/AMPDiscover/AMPDiscover.csv"
+esm2_representation="esm2_t36",
+pdb_path="datasets/AMPDiscover/ESMFold_pdbs/"
+edge_construction_functions="distance_threshold"
+distance_function="euclidean"
+threshold=10
+granularity="CA"
+number_of_heads=8
+hidden_layer_dimension=128
+add_self_loops=True
+use_edge_attr=False
+dropout_rate=0.5
+batch_size=512
+model_path="output_models/AMPDiscover/amp_esmt36_d10_hd128/amp_esmt36_d10_hd128.pt"
+log_filename="TrainingLog_AMPDiscover"
+prediction_filename="PredictionLog_AMPDiscover"
+
 
 python test.py \
-    --dataset datasets/AMPDiscover/AMPDiscover.csv \
+    --dataset "$dataset" \
     --esm2_representation "$esm2_representation" \
-    --tertiary_structure_method esmfold \
-    --tertiary_structure_path datasets/AMPDiscover/ESMFold_pdbs/ \
-    --b 512 \
-    --trained_model_path "$trained_model_path" \
-    --drop 0.5 \
-    --hd "$hd" \
-    --heads 8 \
-    --d "$d" \
-    --log_file_name "$log_file_name" \
-    --test_result_file_name "$test_result_file_name"
+    --pdb_path="$pdb_path"
+    --edge_construction_functions="$edge_construction_functions"
+    --distance_function="$distance_function"
+    --threshold="$threshold"
+    --granularity="$granularity"
+    --number_of_heads="$number_of_heads"
+    --hidden_layer_dimension="$hidden_layer_dimension"
+    --add_self_loops="$add_self_loops"
+    --use_edge_attr="$use_edge_attr"
+    --learning_rate="$learning_rate"
+    --dropout_rate="$dropout_rate"
+    --batch_size="$batch_size"
+    --number_of_epoch="$number_of_epoch"
+    --model_path="$model_path"
+    --log_filename="$log_filename"
 
--- train.sh
+++ train.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
 
-e=200
-hd=128
-d=10
-esm2_representation="esm2_t36"
-path_to_save_models="output_models/AMPDiscover/amp_esmt36_d10_hd128/"
-log_file_name="TrainingLog_AMPDiscover"
+dataset="datasets/AMPDiscover/AMPDiscover.csv"
+esm2_representation="esm2_t36",
+tertiary_structure_method="esmfold"
+edge_construction_functions="distance_threshold"
+distance_function="euclidean"
+threshold=10
+granularity="CA"
+number_of_heads=8
+hidden_layer_dimension=128
+add_self_loops=True
+use_edge_attr=False
+learning_rate=0.0001
+dropout_rate=0.25
+batch_size=512
+number_of_epoch=200
+model_path="output_models/AMPDiscover/amp_esmt36_d10_hd128/"
+log_filename="TrainingLog_AMPDiscover"
+
 
 python train.py \
-    --dataset datasets/AMPDiscover/AMPDiscover.csv \
+    --dataset "$dataset" \
     --esm2_representation "$esm2_representation" \
-    --tertiary_structure_method esmfold \
-    --tertiary_structure_path datasets/AMPDiscover/ESMFold_pdbs/ \
-    --e "$e" \
-    --hd "$hd" \
-    --path_to_save_models "$path_to_save_models" \
-    --d "$d" \
-    --log_file_name "$log_file_name"
+    --tertiary_structure_method="$tertiary_structure_method"
+    --edge_construction_functions="$edge_construction_functions"
+    --distance_function="$distance_function"
+    --threshold="$threshold"
+    --granularity="$granularity"
+    --number_of_heads="$number_of_heads"
+    --hidden_layer_dimension="$hidden_layer_dimension"
+    --add_self_loops="$add_self_loops"
+    --use_edge_attr="$use_edge_attr"
+    --learning_rate="$learning_rate"
+    --dropout_rate="$dropout_rate"
+    --batch_size="$batch_size"
+    --number_of_epoch="$number_of_epoch"
+    --model_path="$model_path"
+    --log_filename="$log_filename"
+
+
+
 
