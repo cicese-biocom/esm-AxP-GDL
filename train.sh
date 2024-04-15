b1b7992731de1dd9e3efbe23174c8b2cca1968bc
Greneter Cordoves Delgado <greneter@cicese.edu.mx>
2024-04-15 11:57:35 -0700

    feat(train.sh): Add example to train a model

--- train.sh
+++ train.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
-e=200
-hd=128
-d=10
-esm2_representation="esm2_t36"
-path_to_save_models="output_models/AMPDiscover/amp_esmt36_d10_hd128/"
-log_file_name="TrainingLog_AMPDiscover"
+dataset="datasets/TestDataset/TestDataset.csv"
+pdb_path="datasets/TestDataset/ESMFold_pdbs/"
+tertiary_structure_method='esmfold'
+gdl_model_path="output/TestDataset/amp_esmt33_d10_hd128/"
+esm2_representation="esm2_t33"
+edge_construction_functions="distance_based_threshold"
+distance_function="euclidean"
+distance_threshold=10
+amino_acid_representation="CA"
+number_of_heads=8
+hidden_layer_dimension=128
+learning_rate=0.0001
+dropout_rate=0.25
+batch_size=512
+number_of_epoch=5
 
 python train.py \
-    --dataset datasets/AMPDiscover/AMPDiscover.csv \
+    --dataset "$dataset" \
+    --pdb_path "$pdb_path" \
+    --tertiary_structure_method="$tertiary_structure_method"  \
+    --gdl_model_path="$gdl_model_path"  \
     --esm2_representation "$esm2_representation" \
-    --tertiary_structure_method esmfold \
-    --tertiary_structure_path datasets/AMPDiscover/ESMFold_pdbs/ \
-    --e "$e" \
-    --hd "$hd" \
-    --path_to_save_models "$path_to_save_models" \
-    --d "$d" \
-    --log_file_name "$log_file_name"
+    --edge_construction_functions="$edge_construction_functions" \
+    --distance_function="$distance_function" \
+    --distance_threshold="$distance_threshold" \
+    --amino_acid_representation="$amino_acid_representation" \
+    --number_of_heads="$number_of_heads" \
+    --hidden_layer_dimension="$hidden_layer_dimension" \
+    --add_self_loops \
+    --use_edge_attr \
+    --learning_rate="$learning_rate" \
+    --dropout_rate="$dropout_rate" \
+    --batch_size="$batch_size" \
+    --number_of_epoch="$number_of_epoch"
 
