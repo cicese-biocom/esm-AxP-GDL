0446f45da449a74452c7624dc7d4d289b0564557
Greneter Cordoves Delgado <greneter@cicese.edu.mx>
2024-04-15 16:50:28 -0700

    refactor(train.py): Remove the function set_of_edge_functions

--- train.py
+++ train.py
@@ -1,13 +1,6 @@
-from pathlib import Path
 from workflow.args_parser_handler import ArgsParserHandler
 from workflow.gdl_workflow import TrainingWorkflow
 from workflow.application_context import ApplicationContext
-
-
-def set_of_edge_functions(value):
-    functions = value.replace(" ", "").split(',')
-    functions = set(functions)
-    return functions
 
 
 def train(args):
