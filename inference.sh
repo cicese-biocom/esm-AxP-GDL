fb11c643b4db32ffd1e78cf6ec95fd1600b711c1
Greneter Cordoves Delgado <greneter@cicese.edu.mx>
2024-04-15 22:00:28 -0700

    feat(inference.sh): Add example to use a model for inference

--- inference.sh
+++ inference.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-dataset="example/dataset/ExampleDataset.csv"
-pdb_path="example/dataset/ESMFold_pdbs/"
+dataset="example/ExampleDataset.csv"
+pdb_path="example/ESMFold_pdbs/"
 gdl_model_path="example/output/Checkpoints/gdl_model_name.pt"
 output_path="example/output/"
 dropout_rate=0.25
@@ -14,4 +14,3 @@ python test.py \
     --output_path="$output_path"  \
     --dropout_rate="$dropout_rate" \
     --batch_size="$batch_size"
-
