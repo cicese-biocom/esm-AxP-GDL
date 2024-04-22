6739be91df68b4cf72f53ffb2cf891ff960d927b
Greneter Cordoves Delgado <greneter@cicese.edu.mx>
2024-04-22 10:21:11 -0700

    feat(edges): Add the parameter use_edge_attr to the call arguments of the compute_edges function.

--- graph/edges.py
+++ graph/edges.py
@@ -14,27 +14,27 @@ def get_edges(workflow_settings: ParameterSetter, data: pd.DataFrame, esm2_conta
     else:
         atom_coordinates_matrices, data = load_tertiary_structures(workflow_settings, data)
 
-    adjacency_matrices, weights_matrices = _construct_edges(atom_coordinates_matrices, esm2_contact_maps, workflow_settings)
+    adjacency_matrices, weights_matrices = _construct_edges(atom_coordinates_matrices,
+                                                            esm2_contact_maps,
+                                                            workflow_settings)
     return adjacency_matrices, weights_matrices, data
 
 
 def _construct_edges(atom_coordinates_matrices: np.array, esm2_contact_maps, workflow_settings: ParameterSetter):
     num_cores = multiprocessing.cpu_count()
 
+    if not workflow_settings.use_esm2_contact_map:
+        esm2_contact_maps = [None] * len(atom_coordinates_matrices)
+
     if workflow_settings.use_esm2_contact_map:
         args = [(workflow_settings.edge_construction_functions,
                  workflow_settings.distance_function,
                  workflow_settings.distance_threshold,
                  esm2_contact_map,
-                 atom_coordinates) for (atom_coordinates, esm2_contact_map) in
+                 atom_coordinates,
+                 workflow_settings.use_edge_attr
+                 ) for (atom_coordinates, esm2_contact_map) in
                 zip(atom_coordinates_matrices, esm2_contact_maps)]
-    else:
-        args = [(workflow_settings.edge_construction_functions,
-                 workflow_settings.distance_function,
-                 workflow_settings.distance_threshold,
-                 None,
-                 atom_coordinates) for atom_coordinates in
-                atom_coordinates_matrices]
 
     with ProcessPoolExecutor(max_workers=num_cores) as pool:
         with tqdm(range(len(args)), total=len(args), desc="Generating adjacency matrices", disable=False) as progress:
