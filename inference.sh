4a1061ba620e9ef52a2f2d7872717adce7fe92de
Greneter Cordoves Delgado <greneter@cicese.edu.mx>
2024-10-09 16:37:03 -0700

    chore(inference.sh): Change gdl model name to gdl_model_name.pt

--- inference.sh
+++ inference.sh
@@ -2,7 +2,7 @@
 
 dataset="example/ExampleDatasetInference.csv"
 pdb_path="example/ESMFold_pdbs/"
-gdl_model_path="example/output/Checkpoints/epoch=2_train-loss=0.70_val-loss=0.70_(best-mcc).pt"
+gdl_model_path="example/output/Checkpoints/gdl_model_name.pt"
 output_path="example/output/"
 batch_size=512
 
