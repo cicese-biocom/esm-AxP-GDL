58a3c22407f507543b1e820df628057c927ec99c
Greneter Cordoves Delgado <greneter@cicese.edu.mx>
2024-10-09 14:13:59 -0700

    perf(dropout_rate parameter): Set dropout_rate parameter as a sensitive parameter
    Save the dropout rate in the trained .pt model

--- inference.sh
+++ inference.sh
@@ -2,11 +2,11 @@
 
 dataset="example/ExampleDatasetInference.csv"
 pdb_path="example/ESMFold_pdbs/"
-gdl_model_path="example/output/Checkpoints/epoch=2_train-loss=0.70_val-loss=0.70_(best-mcc).pt"
+gdl_model_path="example/output/Checkpoints/gdl_model_name.pt"
 output_path="example/output/"
 batch_size=512
 
-python test.py \
+python inference.py \
     --dataset "$dataset" \
     --pdb_path "$pdb_path" \
     --gdl_model_path="$gdl_model_path" \
