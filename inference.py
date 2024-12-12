7b575c9fe205c1e3af63117d7e05ed13eb073f7c
Greneter Cordoves Delgado <greneter@cicese.edu.mx>
2024-12-11 20:42:06 -0800

    refactor(exception): Write all errors to the log

--- inference.py
+++ inference.py
@@ -1,3 +1,5 @@
+import logging
+
 from workflow.application_context import ApplicationContext
 from workflow.args_parser_handler import ArgsParserHandler
 from workflow.gdl_workflow import InferenceWorkflow
@@ -5,21 +7,22 @@ import time
 
 
 def inference(args):
-    try:
-        context = ApplicationContext(mode='inference')
-        InferenceWorkflow().run_workflow(context=context, parameters=args)
-
-    except Exception as e:
-        raise e
+    context = ApplicationContext(mode='inference')
+    InferenceWorkflow().run_workflow(context=context, parameters=args)
 
 
 if __name__ == '__main__':
-    args_handler = ArgsParserHandler()
-    args = args_handler.get_inference_arguments()
+    try:
+        args_handler = ArgsParserHandler()
+        args = args_handler.get_inference_arguments()
 
-    start_time = time.time()
-    inference(args)
-    final_time = time.time()
-    print(
-        f"Inference execution time in: {str(final_time - start_time)} seconds"
-    )
+        start_time = time.time()
+        inference(args)
+        final_time = time.time()
+        print(
+            f"Inference execution time in: {str(final_time - start_time)} seconds"
+        )
+
+    except Exception as e:
+        logging.getLogger('workflow_logger').critical(e)
+        quit()
