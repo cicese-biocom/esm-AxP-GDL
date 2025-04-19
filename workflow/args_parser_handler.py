7a96388d83c449f2c07013d54404c99485061f98
Greneter Cordoves Delgado <greneter@cicese.edu.mx>
2025-04-19 12:46:36 -0700

    perf(args): Modify parameter loading logic to support JSON and CLI input
    Allow parameters to be specified both from a JSON file and directly via the command line.
    
    Explicitly defined command-line parameters take precedence over those specified in the JSON file.
    
    Raise an exception if any invalid arguments are provided.

--- workflow/args_parser_handler.py
+++ workflow/args_parser_handler.py
@@ -20,135 +20,155 @@ def methods_for_ad():
 
 class ArgsParserHandler:
     def __init__(self):
-        self.parser = ArgumentParser()
+        self._parser = ArgumentParser()
         check_json_params_arg()
 
+    def _check_unknown_args(self):
+        temp_parser = ArgumentParser(add_help=False)
+        for action in self._parser._actions:
+            for opt in action.option_strings:
+                temp_parser.add_argument(opt)
+
+        _, unknown_args = temp_parser.parse_known_args()
+
+        if unknown_args:
+            raise ValueError(f"Unknown arguments: {unknown_args}")
+
     def _add_common_arguments(self):
-        self.parser.add_argument('--dataset', type=Path, required=True, help='Path to the input dataset in CSV format')
+        self._parser.add_argument('--dataset', type=Path, required=True, help='Path to the input dataset in CSV format')
 
-        self.parser.add_argument('--tertiary_structure_method', type=str, default=None,
-                                 choices=['esmfold'],
-                                 help='3D structure prediction method. None indicates to load existing tertiary '
-                                      'structures from PDB files , otherwise, sequences in input CSV file are '
-                                      'predicted using the specified method')
+        self._parser.add_argument('--tertiary_structure_method', type=str, default=None,
+                                  choices=['esmfold'],
+                                  help='3D structure prediction method. None indicates to load existing tertiary '
+                                       'structures from PDB files , otherwise, sequences in input CSV file are '
+                                       'predicted using the specified method')
 
-        self.parser.add_argument('--pdb_path', type=Path, default=None,
-                                 help='Path where tertiary structures are saved in or loaded from PDB files')
+        self._parser.add_argument('--pdb_path', type=Path, default=None,
+                                  help='Path where tertiary structures are saved in or loaded from PDB files')
 
-        self.parser.add_argument('--batch_size', type=int, default=512, help='Batch size')
+        self._parser.add_argument('--batch_size', type=int, default=512, help='Batch size')
 
-        self.parser.add_argument('--gdl_model_path', type=Path, required=True,
-                                 help=' The path to save/load the models')
+        self._parser.add_argument('--gdl_model_path', type=Path, required=True,
+                                  help='The path to save/load the models')
+
+        self._parser.add_argument('--command_line_params', type=Path, default=None,
+                                  help='Path to a JSON file with the parameters to be used from the command line. '
+                                       'Arguments provided directly via the command line take precedence over those '
+                                       'specified in this file.')
 
     def _add_eval_arguments(self) -> Dict:
         self._add_common_arguments()
 
-        self.parser.add_argument('--output_path', type=Path, required=True,
-                                 help='The path where the output data will be saved.')
+        self._parser.add_argument('--output_path', type=Path, required=True,
+                                  help='The path where the output data will be saved.')
 
-        self.parser.add_argument('--seed', type=int, default=None,
-                                 help='Seed to run the Test/Inference mode')
+        self._parser.add_argument('--seed', type=int, default=None,
+                                  help='Seed to run the Test/Inference mode')
 
-        self.parser.add_argument('--methods_for_ad', type=set_of_functions, default=None,
-                                 help=f"Methods to build applicability domain model. The options available are: "
-                                      f"{methods_for_ad()}")
+        self._parser.add_argument('--methods_for_ad', type=set_of_functions, default=None,
+                                  help=f"Methods to build applicability domain model. The options available are: "
+                                       f"{methods_for_ad()}")
 
-        self.parser.add_argument('--feature_file_for_ad', type=Path, default=None,
-                                 help='Path of the CSV file of features to build the applicability domain.')
+        self._parser.add_argument('--feature_file_for_ad', type=Path, default=None,
+                                  help='Path of the CSV file of features to build the applicability domain.')
 
     def get_training_arguments(self) -> Dict:
         self._add_common_arguments()
 
         # See 10.1002/pro.4928 for reduced and combined representations
-        self.parser.add_argument('--esm2_representation', type=str, default='esm2_t33',
-                                 choices=['esm2_t6', 'esm2_t12', 'esm2_t30', 'esm2_t33', 'esm2_t36', 'esm2_t48',
-                                          'reduced_esm2_t6', 'reduced_esm2_t12', 'reduced_esm2_t30', 'reduced_esm2_t33',
-                                          'reduced_esm2_t36', 'combined_esm2'],
-                                 help='ESM-2 representation to be used')
+        self._parser.add_argument('--esm2_representation', type=str, default='esm2_t33',
+                                  choices=['esm2_t6', 'esm2_t12', 'esm2_t30', 'esm2_t33', 'esm2_t36', 'esm2_t48',
+                                           'reduced_esm2_t6', 'reduced_esm2_t12', 'reduced_esm2_t30',
+                                           'reduced_esm2_t33',
+                                           'reduced_esm2_t36', 'combined_esm2'],
+                                  help='ESM-2 representation to be used')
 
-        self.parser.add_argument('--edge_construction_functions', type=set_of_functions, default=None,
-                                 help="Criteria (e.g., distance) to define a relationship (graph edges) between amino "
-                                      "acids. The options available are: 'distance_based_threshold', "
-                                      "'sequence_based', 'esm2_contact_map'")
+        self._parser.add_argument('--edge_construction_functions', type=set_of_functions, default=None,
+                                  help="Criteria (e.g., distance) to define a relationship (graph edges) between amino "
+                                       "acids. The options available are: 'distance_based_threshold', "
+                                       "'sequence_based', 'esm2_contact_map'")
 
-        self.parser.add_argument('--distance_function', type=str, default=None,
-                                 choices=['euclidean', 'canberra', 'lance_williams', 'clark', 'soergel',
-                                          'bhattacharyya',
-                                          'angular_separation'],
-                                 help='Distance function to construct the edges of the distance-based graph')
+        self._parser.add_argument('--distance_function', type=str, default=None,
+                                  choices=['euclidean', 'canberra', 'lance_williams', 'clark', 'soergel',
+                                           'bhattacharyya',
+                                           'angular_separation'],
+                                  help='Distance function to construct the edges of the distance-based graph')
 
-        self.parser.add_argument('--distance_threshold', type=float, default=None,
-                                 help='Distance threshold to construct the edges of the distance-based graph')
+        self._parser.add_argument('--distance_threshold', type=float, default=None,
+                                  help='Distance threshold to construct the edges of the distance-based graph')
 
-        self.parser.add_argument('--esm2_model_for_contact_map', type=str, default=None,
-                                 choices=['esm2_t6', 'esm2_t12', 'esm2_t30', 'esm2_t33', 'esm2_t36', 'esm2_t48'],
-                                 help='ESM-2 model to be used to obtain ESM-2 contact map')
+        self._parser.add_argument('--esm2_model_for_contact_map', type=str, default=None,
+                                  choices=['esm2_t6', 'esm2_t12', 'esm2_t30', 'esm2_t33', 'esm2_t36', 'esm2_t48'],
+                                  help='ESM-2 model to be used to obtain ESM-2 contact map')
 
-        self.parser.add_argument('--probability_threshold', type=float, default=None,
-                                 help='Probability threshold for constructing a graph based on ESM-2 contact maps. '
-                                      'It takes a value between 0.5 and 1.0.')
+        self._parser.add_argument('--probability_threshold', type=float, default=None,
+                                  help='Probability threshold for constructing a graph based on ESM-2 contact maps. '
+                                       'It takes a value between 0.5 and 1.0.')
 
-        self.parser.add_argument('--amino_acid_representation', type=str, default='CA',
-                                 choices=['CA'],
-                                 help='Reference atom into an amino acid to define a relationship (e.g., distance) '
-                                      'regarding another amino acid')
+        self._parser.add_argument('--amino_acid_representation', type=str, default='CA',
+                                  choices=['CA'],
+                                  help='Reference atom into an amino acid to define a relationship (e.g., distance) '
+                                       'regarding another amino acid')
 
-        self.parser.add_argument('--number_of_heads', type=int, default=8, help='Number of heads')
+        self._parser.add_argument('--number_of_heads', type=int, default=8, help='Number of heads')
 
-        self.parser.add_argument('--hidden_layer_dimension', type=int, default=128, help='Hidden layer dimension')
+        self._parser.add_argument('--hidden_layer_dimension', type=int, default=128, help='Hidden layer dimension')
 
-        self.parser.add_argument('--add_self_loops', action="store_true",
-                                 help="True if specified, otherwise, False. True indicates to use auto loops in "
-                                      "attention layer.")
+        self._parser.add_argument('--add_self_loops', action="store_true",
+                                  help="True if specified, otherwise, False. True indicates to use auto loops in "
+                                       "attention layer.")
 
-        self.parser.add_argument('--use_edge_attr', action="store_true",
-                                 help="True if specified, otherwise, False. True indicates to use edge attributes in "
-                                      "graph "
-                                      "learning.")
+        self._parser.add_argument('--use_edge_attr', action="store_true",
+                                  help="True if specified, otherwise, False. True indicates to use edge attributes in "
+                                       "graph "
+                                       "learning.")
 
-        self.parser.add_argument('--learning_rate', type=float, default=1e-4, help='Learning rate')
+        self._parser.add_argument('--learning_rate', type=float, default=1e-4, help='Learning rate')
 
-        self.parser.add_argument('--dropout_rate', type=float, default=0.25, help='Dropout rate')
+        self._parser.add_argument('--dropout_rate', type=float, default=0.25, help='Dropout rate')
 
-        self.parser.add_argument('--number_of_epochs', type=int, default=200, help='Maximum number of epochs')
+        self._parser.add_argument('--number_of_epochs', type=int, default=200, help='Maximum number of epochs')
 
-        self.parser.add_argument('--save_ckpt_per_epoch', action="store_true",
-                                 help="True if specified, otherwise, False. True indicates that the models of every "
-                                      "epoch will be saved. False indicates that the latest model and the best model "
-                                      "regarding the MCC metric will be saved.")
+        self._parser.add_argument('--save_ckpt_per_epoch', action="store_true",
+                                  help="True if specified, otherwise, False. True indicates that the models of every "
+                                       "epoch will be saved. False indicates that the latest model and the best model "
+                                       "regarding the MCC metric will be saved.")
 
-        self.parser.add_argument('--validation_mode', type=str, default=None,
-                                 choices=['random_coordinates', 'random_embeddings'],
-                                 help='Criteria to validate that the predictions of the models are not by chance')
+        self._parser.add_argument('--validation_mode', type=str, default=None,
+                                  choices=['random_coordinates', 'random_embeddings'],
+                                  help='Criteria to validate that the predictions of the models are not by chance')
 
-        self.parser.add_argument('--randomness_percentage', type=float, default=None,
-                                 help='Percentage of rows to be randomly generated. This parameter and the --validation_mode parameter are used together.')
+        self._parser.add_argument('--randomness_percentage', type=float, default=None,
+                                  help='Percentage of rows to be randomly generated. This parameter and the --validation_mode parameter are used together.')
 
-        self.parser.add_argument('--split_method', type=str, default=None,
-                                 choices=['random', 'expectation_maximization'],
-                                 help='Method to split an input dataset in training and validation sets. This parameter is used when an used-defined validation set is not given. To use this parameter, all no-test instances must be marked as training, i.e., value 1 in the input CSV file.')
+        self._parser.add_argument('--split_method', type=str, default=None,
+                                  choices=['random', 'expectation_maximization'],
+                                  help='Method to split an input dataset in training and validation sets. This parameter is used when an used-defined validation set is not given. To use this parameter, all no-test instances must be marked as training, i.e., value 1 in the input CSV file.')
 
-        self.parser.add_argument('--split_training_fraction', type=float, default=None,
-                                 help='If the --split_method is specified, this parameter represents the percentage of instances '
-                                      'to be considered as training. The other ones will be allocated in the validation set. '
-                                      'It takes a value between 0.6 and 0.9.')
+        self._parser.add_argument('--split_training_fraction', type=float, default=None,
+                                  help='If the --split_method is specified, this parameter represents the percentage of instances '
+                                       'to be considered as training. The other ones will be allocated in the validation set. '
+                                       'It takes a value between 0.6 and 0.9.')
 
-        args = self.parser.parse_args()
+        self._check_unknown_args()
+        args = self._parser.parse_args()
         return vars(args)
 
     def get_eval_arguments(self) -> Dict:
         self._add_eval_arguments()
 
-        args = self.parser.parse_args()
+        self._check_unknown_args()
+        args = self._parser.parse_args()
         return vars(args)
 
     def get_inference_arguments(self) -> Dict:
         self._add_eval_arguments()
 
-        self.parser.add_argument('--inference_batch_size', type=int, default=20000,
-                                 help='As the inference data are unlimited, this parameters contains the number of instances to be processed in a specific chunk (batch) at a time.')
+        self._parser.add_argument('--inference_batch_size', type=int, default=20000,
+                                  help='As the inference data are unlimited, this parameters contains the number of instances to be processed in a specific chunk (batch) at a time.')
 
-        args = self.parser.parse_args()
+        self._check_unknown_args()
+        args = self._parser.parse_args()
         return vars(args)
 
 
@@ -156,24 +176,20 @@ def check_json_params_arg():
     user_args = sys.argv[1:]
 
     if '--command_line_params' in user_args:
-        if len(user_args) > 2:
-            raise ValueError("--command_line_params cannot be used together with any other parameters. "
-                             "Please either provide this path or specify parameters individually, not both.")
-
         json_file_path = Path(user_args[1]).resolve()
         json_args = load_json(json_file_path)
 
         argv = []
         for key, value in json_args.items():
-            if isinstance(value, bool):
-                if value:
-                    argv.append(f"--{key}")
-            elif isinstance(value, list):
-                if value:
-                    comma_separated = ",".join(map(str, value))
-                    argv.extend([f"--{key}", comma_separated])
-            elif value is not None:
-                argv.extend([f"--{key}", str(value)])
+            if f"--{key}" not in user_args:
+                if isinstance(value, bool):
+                    if value:
+                        argv.append(f"--{key}")
+                elif isinstance(value, list):
+                    if value:
+                        comma_separated = ",".join(map(str, value))
+                        argv.extend([f"--{key}", comma_separated])
+                elif value is not None:
+                    argv.extend([f"--{key}", str(value)])
 
-        sys.argv = [sys.argv[0]] + argv
-
+        sys.argv = [sys.argv[0]] + argv + user_args
