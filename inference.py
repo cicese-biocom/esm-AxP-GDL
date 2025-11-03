115f7e5e34832097caf73109a69f33eb88358256
Greneter Cordoves Delgado <greneter@cicese.edu.mx>
2025-11-03 11:41:51 -0800

    refactor: Remove try-except block from main execution flow

--- inference.py
+++ inference.py
@@ -12,16 +12,13 @@ def inference(args):
 
 
 if __name__ == '__main__':
-    try:
-        args_handler = ArgsParserHandler()
-        args = args_handler.get_inference_arguments()
+    args_handler = ArgsParserHandler()
+    args = args_handler.get_inference_arguments()
 
-        start_time = time.time()
-        inference(args)
-        final_time = time.time()
-        logging.getLogger('workflow_logger').info(
-            f"Inference execution time in: {str(final_time - start_time)} seconds")
+    start_time = time.time()
+    inference(args)
+    final_time = time.time()
+    logging.getLogger('workflow_logger').info(
+        f"Inference execution time in: {str(final_time - start_time)} seconds")
 
-    except Exception as e:
-        logging.getLogger('workflow_logger').critical(e)
-        quit()
+
